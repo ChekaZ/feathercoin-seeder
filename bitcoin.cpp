a9f136f1ee0740e22147d5ac52c5f99b39cb1ec8
Oliver Ziegler <o.zieglerZ@web.de>
2018-03-31 02:07:13 +0200

    Trezarcoin init

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -81,7 +81,7 @@ class CNode {
     CAddress me(CService("0.0.0.0"));
     BeginMessage("version");
     int nBestHeight = GetRequireHeight();
-    string ver = "/feathercoin-seeder:0.01/";
+    string ver = "/Trezarcoin-seeder:0.01/";
     vSend << PROTOCOL_VERSION << nLocalServices << nTime << you << me << nLocalNonce << ver << nBestHeight;
     EndMessage();
   }
@@ -302,7 +302,7 @@ bool TestNode(const CService &cip, int &ban, int &clientV, std::string &clientSV
 
 /*
 int main(void) {
-  CService ip("www.feathercoin.com", 9336, true);
+  CService ip("www.Trezarcoin.com", 17298, true);
   vector<CAddress> vAddr;
   vAddr.clear();
   int ban = 0;
